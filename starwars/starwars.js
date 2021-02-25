fe8094c56d01e962cddb20f753459a44998fa31e
Anferny Mendoza <anferny.mendoza@icloud.com>
2021-02-25 12:54:03 -0700

    starwars+Films1-2

---
+++ starwars/starwars.js
@@ -0,0 +1,13 @@
+
+import { films } from '../data/films.js'
+
+
+console.log(films[6]);
+
+let filmOne = document.querySelector('#film1')
+let filmTwo = document.querySelector('#film2')
+
+
+filmOne.textContent = films[2].title
+filmTwo.textContent = films[1].title
+
