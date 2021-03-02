c509b53f56c46f79178c65ae8f398250613cb64b
Anferny Mendoza <anferny.mendoza@icloud.com>
2021-03-02 11:11:13 -0700

    StarWarsPostersUp

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -2,12 +2,31 @@
 import { films } from '../data/films.js'
 
 
-console.log(films[6]);
-
-let filmOne = document.querySelector('#film1')
-let filmTwo = document.querySelector('#film2')
 
 
-filmOne.textContent = films[2].title
-filmTwo.textContent = films[1].title
+let filmList = document.querySelector('#filmList')
 
+
+for (let i = 0; i < films.length; i++) {
+    console.log(`The loop counter is: ${i} while the film episode id is
+    : ${films[i].episode_id}`)
+    
+    //let lastNum = getLastNumber(films[i].url)
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
+    let postFig = document.createElement("figure")
+    let figImg = document.createElement("img")
+    figImg.src = "https://starwars-visualguide.com/assets/img/films/7.jpg"
+    let figCaption = document.createElement("figcaption")
+
+    figCaption.textContent = foundFilm.title
+    postFig.appendChild(figImg)
+    postFig.appendChild(figCaption)
+
+    filmList.appendChild(postFig)
+
+}
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf("/")
+    return url.charAt(end - 1)
+}
