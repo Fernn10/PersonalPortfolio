1b42e4fbbe16f94c557927fb5c4c1b60852e9fd1
Anferny Mendoza <anferny.mendoza@icloud.com>
2021-03-04 10:15:48 -0700

    CharactersPage added

--- films/sw-films.js
+++ films/sw-films.js
@@ -2,20 +2,18 @@
 import { films } from '../data/films.js'
 
 
-
-
 let filmList = document.querySelector('#filmList')
 
 
 for (let i = 0; i < films.length; i++) {
-    console.log(`The loop counter is: ${i} while the film episode id is
-    : ${films[i].episode_id}`)
-    
+
+    //console.log(`The loop counter is: ${i} while the film episode id is
+    //: ${films[i].episode_id}`)
     //let lastNum = getLastNumber(films[i].url)
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
     let postFig = document.createElement("figure")
     let figImg = document.createElement("img")
-    figImg.src = "https://starwars-visualguide.com/assets/img/films/7.jpg"
+    figImg.src = 'https://starwars-visualguide.com/assets/img/films/5.jpg'
     let figCaption = document.createElement("figcaption")
 
     figCaption.textContent = foundFilm.title
